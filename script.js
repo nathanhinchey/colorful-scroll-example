027d7b487bddece92049223fce768620c533c09f
Nathan Hinchey <nathanhinchey@gmail.com>
2016-12-16 14:40:52 -0500

    Rainbow section with buttons!

--- script.js
+++ script.js
@@ -1,58 +1,12 @@
-var sections = ["#s1", "#s2", "#s3", "#s4", "#s5", "#s6"];
-var sectionId = 0;
+var currentSection = 0;
 
-var log = function(){
-  $("#pseudo-console").html(
-    [
-      sections[sectionId] + " position().top: " + $(sections[sectionId]).position().top,
-      sections[sectionId] + " offset().top: " + $(sections[sectionId]).offset().top,
-      "#container position.top: " + $("#container").position().top,
-      "#container offset.top: " + $("#container").offset().top,
-      
-    ].join("<br>")
-  );
+$("button").click(function(event){ 
+  scrollTo(event.currentTarget.value)
+});
+
+var scrollTo = function(sectionNumber){
+  $("#container").animate({
+      scrollTop: sectionNumber * $("#container").height(),
+  }, 200 + 100 * Math.abs(currentSection - sectionNumber));
+  currentSection = sectionNumber;
 }
-// log();
-// $("#container").click(function(event){
-//   sectionId++;
-//   window.setInterval(log, 1);
-//   $("#container").animate({
-//       scrollTop: $(sections[sectionId]).position().top
-//   }, 200);
-// });
-$("#button1").click(function(event){
-  window.setInterval(log, 1);
-  $("#container").animate({
-      scrollTop: $("#s1").position().top
-  }, 200);
-});
-$("#button2").click(function(event){
-  window.setInterval(log, 1);
-  $("#container").animate({
-      scrollTop: $("#s2").position().top
-  }, 200);
-});
-$("#button3").click(function(event){
-  window.setInterval(log, 1);
-  $("#container").animate({
-      scrollTop: $("#s3").position().top
-  }, 200);
-});
-$("#button4").click(function(event){
-  window.setInterval(log, 1);
-  $("#container").animate({
-      scrollTop: $("#s4").position().top
-  }, 200);
-});
-$("#button5").click(function(event){
-  window.setInterval(log, 1);
-  $("#container").animate({
-      scrollTop: $("#s5").position().top
-  }, 200);
-});
-$("#button6").click(function(event){
-  window.setInterval(log, 1);
-  $("#container").animate({
-      scrollTop: $("#s6").position().top
-  }, 200);
-});
